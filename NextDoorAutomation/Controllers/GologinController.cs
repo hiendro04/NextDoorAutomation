370558903a3b8f978d6082f9ebe1501e5e78391a
ProjectSchool <baclinh0123@gmail.com>
2024-12-07 00:05:13 +0700

    update

---
+++ NextDoorAutomation/Controllers/GologinController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NextDoorAutomation.Controllers
+{
+    public class GologinController : Controller
+    {
+        public GologinController() { }
+        public async Task<IActionResult> Index()
+        {
+
+            var test = new Test();
+            await test.GetData();
+            Console.WriteLine("---");
+            return View();
+        }
+    }
+}
--- NextDoorAutomation/Controllers/HomeController.cs
+++ NextDoorAutomation/Controllers/HomeController.cs
@@ -6,16 +6,14 @@ namespace NextDoorAutomation.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController(ILogger<HomeController> logger)
-        {
-        }
+        public HomeController() { }
 
         public async Task<IActionResult> Index()
         {
 
-            var test = new Test();
-            await test.GetData();
-            Console.WriteLine("---");
+            //var test = new Test();
+            //await test.GetData();
+            //Console.WriteLine("---");
             return View();
         }
 
